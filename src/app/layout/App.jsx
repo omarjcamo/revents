5ca4db9daab0fb9b314d1c16a9fdff165eb27d51
Omar Camarero <omarjcamo@gmail.com>
2018-10-25 19:30:40 +0300

    Setting up static components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
+import {Container} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-<h1>Re-Events</h1>
+      <div>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
